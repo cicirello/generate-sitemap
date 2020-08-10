b6c3d9aa3c9cdabe9124f7c4b89480200cc112bc
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2020-08-10 10:57:02 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@ else
 fi
 
 if [ "$includeHTML" == "true" ]; then
-	find . \( -name '*.html' -o -name '*.htm' \) -type f -printf '%h\0%d\0%p\n' | sort -t '\0' -n | awk -F '\0' '{print $3}' | while read i; do 
+	for i in $(find . \( -name '*.html' -o -name '*.htm' \) -type f -printf '%h\0%d\0%p\n' | sort -t '\0' -n | awk -F '\0' '{print $3}'); do 
 		echo "$i"
 		if [ "0" == $(grep -i -c -E "<meta*.*name*.*robots*.*content*.*noindex" $i || true) ]; then
 			lastMod=$(git log -1 --format=%cI $i)
